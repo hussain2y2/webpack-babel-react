024a7cdb812bf2d0b4b46cc6c2d8d3421de88623
Hussain Ahmad <engrhussainahmad@gmail.com>
2020-11-13 01:22:27 +0500

    Structure of Webpack-React

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 class Home extends Component {
+  constructor() {
+    super();
+    // this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick = (param) => {
+    console.log(param)
+  }
   render() {
     return(
       <div className="home">
         <h4>Home Page</h4>
+        <button className="btn btn-sm btn-primary" onClick={(event) => this.handleClick('Testing')}>Click Me</button>
       </div>
     )
   }
--- src/pages/order/reducers/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const ADD_ITEM = 'ADD_ORDER'
-export const REMOVE_ITEM = 'REMOVE_ORDER'
